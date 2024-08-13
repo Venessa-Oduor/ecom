449c084af2c130dea6bda18329f42cddc97eab07
Developer_Venessa <venessaanyango20@gmail.com>
2024-08-13 07:34:59 +0300

    updated

--- Store/models.py
+++ Store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -28,3 +29,14 @@ class Product(models.Model):
         return self.title
 
 
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(max_length=12)
+    zipcode = models.IntegerField()
+    # state = models.CharField(choices=STATE_CHOICES, max_length=100) if you want to add counties of a country
+
+    def __str__(self):
+        return self.name
--- Store/views.py
+++ Store/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
-from .models import Product
-from .forms import CustomerRegistrationForm
+from .models import Product, Customer
+from .forms import CustomerRegistrationForm, CustomerProfileForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -24,6 +26,9 @@ class CategoryView(View):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title')
         return render(request, "app/category.html", locals())
+    # locals() is used to access the local symbol table in Python.
+    # Symbol table: It is a data structure created by a compiler
+    # that is used to store all information needed to execute a program.
 
 
 class CategoryTitle(View):
@@ -42,4 +47,35 @@ class ProductDetail(View):
 class CustomerRegistrationView(View):
     def get(self, request):
         form = CustomerRegistrationForm()
-        return render(request, 'app/customerregistration.html', locals())
+
+        return render(request, 'app/customer registration.html', locals())
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # if the user is successfully registered pass the message;Congratulations!
+            messages.success(request, "Congratulations! You have successfully registered.")
+        else:
+            messages.warning(request, "Please try again.")
+        return render(request, 'app/customer registration.html', locals())
+
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', locals())
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user, name=name, locality=locality, city=city, mobile=mobile, zipcode=zipcode)
+            reg.save()
+        return render(request, 'app/profile.html', locals())
